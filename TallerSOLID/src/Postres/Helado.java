4f7f1f5ed70d4492440936896dbedf7a2bcd077f
kevelmun <kevelmun@espol.edu.ec>
2021-06-10 15:47:33 -0500

    v1.1

--- TallerSOLID/src/Postres/Helado.java
+++ TallerSOLID/src/Postres/Helado.java
@@ -12,34 +12,16 @@ import Adicionales.Aderezo;
  *
  * @author Pedro Mendoza
  */
-public class Helado{
-    private String sabor;
-    private double precioParcial;
-    private ArrayList<Aderezo> aderezos;
-    
+public class Helado extends Postre{
     public Helado(String sabor){
-        aderezos= new ArrayList<>();
-        this.sabor=sabor;
-        this.precioParcial = 7.85;
+        super(sabor);
+        setPrecioParcial(7.85);
     }
     
-    public double calcularPrecioFinal(){
-        double precioFinal;
-        precioFinal=(precioParcial+(precioParcial*0.12))+(aderezos.size()*0.50);
-        return precioFinal;
-    }
-
-    public ArrayList<Aderezo> getAderezos() {
-        return aderezos;
-    }
 
     @Override
     public String toString() {
-        return "Helado{" + "sabor=" + sabor + ", precioParcial=" + precioParcial + ", aderezos=" + aderezos + '}';
-    }
-    
-    public String showPrecioFinal(){
-        return "Precio Final: $ " + calcularPrecioFinal();
+        return "Helado{" + "sabor=" + super.getSabor()+ ", precioParcial=" + super.getPrecioParcial()+ ", aderezos=" + super.getAderezos() + '}';
     }
     
     
---
+++ TallerSOLID/src/Postres/Postre.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Postres;
+
+import Adicionales.Aderezo;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Elix
+ */
+public class Postre {
+    private String sabor;
+    private double precioParcial;
+    private ArrayList<Aderezo> aderezos;
+    
+    public Postre(String sabor){
+        aderezos= new ArrayList<>();
+        this.sabor=sabor;
+    }
+    
+    public double calcularPrecioFinal(){
+        double precioFinal;
+        precioFinal=(precioParcial+(precioParcial*0.12))+(aderezos.size()*0.50);
+        return precioFinal;
+    }
+
+    public ArrayList<Aderezo> getAderezos() {
+        return aderezos;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+
+    public double getPrecioParcial() {
+        return precioParcial;
+    }
+
+    public void setPrecioParcial(double precioParcial) {
+        this.precioParcial = precioParcial;
+    }
+    
+    @Override
+    public String toString() {
+        return "Postre{" + "sabor=" + sabor + ", precioParcial=" + precioParcial + ", aderezos=" + aderezos + '}';
+    }
+    public String showPrecioFinal(){
+        return "Precio Final: $ " + calcularPrecioFinal();
+    }
+}
